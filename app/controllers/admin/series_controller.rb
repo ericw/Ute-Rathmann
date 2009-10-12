c8aa4fb0f2b10a6b348dc7829ae57832f9e38bdd
Eric Wahlforss <eric@Forss.local>
2009-10-12 21:20:38 +0200

    adding visible state to series

--- app/controllers/admin/series_controller.rb
+++ app/controllers/admin/series_controller.rb
@@ -2,7 +2,7 @@ class Admin::SeriesController < ApplicationController
   layout "admin"
   active_scaffold :series do |config|
 
-    config.list.columns = [:id, :title, :position]
+    config.list.columns = [:id, :title, :position, :visible]
 
     config.list.per_page = 500
   end
