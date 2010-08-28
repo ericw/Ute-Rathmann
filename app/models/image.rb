27ec6f8568495d3e9602277fdb875923a32dde34
Eric Wahlforss <eric@Forss.local>
2010-08-28 12:19:56 +0200

    bug

--- app/models/image.rb
+++ app/models/image.rb
@@ -18,7 +18,7 @@ class Image < ActiveRecord::Base
   end
   
   def base64_version
-    Base64.encode64(File.open("#{RAILS_ROOT}/public/images/thumbs/#{self.filename}").read)
+    Base64.encode64(File.open("#{RAILS_ROOT}/public/images/thumbs/#{self.filename_thumb}").read)
   end
   
   # save the full size image
